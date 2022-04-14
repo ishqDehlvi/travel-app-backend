43130bc54b027a59601d9446fe745359dfcf4a7d
Inzemam ul-Haq <Inzemamhaq05@gmail.com>
2022-04-14 12:15:06 +0000

    update

--- config/admin.js
+++ config/admin.js
@@ -6,3 +6,11 @@ module.exports = ({ env }) => ({
     salt: env('API_TOKEN_SALT'),
   },
 });
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+});
